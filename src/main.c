bcbe5fb08851ff14f211db7d449aa11e858bbc8e
Chip <chipz68q@gmail.com>
2018-12-08 14:36:57 +0500

    Extract main window creation to subfunc
    Add create_main_window function

--- src/main.c
+++ src/main.c
@@ -1,17 +1,27 @@
 #include <gtk/gtk.h>
 
+static GtkWidget *create_main_window(void);
+
 int main(int argc, char *argv[])
 {
 	GtkWidget	*window;
 
 	gtk_init(&argc, &argv);
 
-	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	window = create_main_window();
 
 	g_signal_connect(G_OBJECT(window), "destroy",
 		G_CALLBACK(gtk_main_quit), NULL);
-	
-	gtk_widget_show_all(window);
 
+	gtk_widget_show_all(window);
 	gtk_main();
+}
+
+static GtkWidget *create_main_window(void)
+{
+	GtkWidget	*window;
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+	return window;
 }
