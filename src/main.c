5984d97011cb52aeb577521db9761022e22ee254
Chip <chipz68q@gmail.com>
2018-12-12 17:37:44 +0500

    Add handler for tree view row activated

--- src/main.c
+++ src/main.c
@@ -18,8 +18,12 @@ static void
 on_button_save_subject_clicked(GtkWidget *button, gpointer data);
 static int db_error(struct ch_sqlite_connection *connection);
 static void load_subject_table(GtkListStore	*store);
+static void on_tree_view_subject_list_row_activated(GtkTreeView *tree_view,
+	GtkTreePath *path, GtkTreeViewColumn *column, gpointer user_data);
 static int add_subject_to_store_cb(void *opt_arg, int col_count,
 	char **cols, char **col_names);
+
+static int selected_subject_id;
 
 static GtkWidget	*window;
 
@@ -67,8 +71,12 @@ static GtkWidget *create_subject_list_page(void)
 	GtkWidget	*frame_controls;
 	GtkWidget	*hbox;
 	GtkWidget	*vbox;
+	GtkWidget	*tree_view_subject_list;
+	GtkWidget	*entry_subject;
 	GtkWidget	*button_save_subject;
 
+	tree_view_subject_list = create_text_view_subject_list();
+	entry_subject = gtk_entry_new();
 	button_save_subject = gtk_button_new_with_label("Сохранить");
 
 	frame_tree_view = gtk_frame_new(NULL);
@@ -84,12 +92,12 @@ static GtkWidget *create_subject_list_page(void)
 
 	gtk_box_pack_start(GTK_BOX(vbox), gtk_label_new("Название предмета:"),
 		FALSE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(vbox), gtk_entry_new(), FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(vbox), entry_subject, FALSE, FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(vbox), button_save_subject,
 		FALSE, FALSE, 0);
 
 	gtk_container_add(GTK_CONTAINER(frame_tree_view),
-		create_text_view_subject_list());
+		tree_view_subject_list);
 	gtk_container_add(GTK_CONTAINER(frame_controls), vbox);
 
 	gtk_box_pack_start(GTK_BOX(hbox), frame_tree_view, TRUE, TRUE, 0);
@@ -98,6 +106,9 @@ static GtkWidget *create_subject_list_page(void)
 	g_signal_connect(G_OBJECT(button_save_subject), "clicked",
 						G_CALLBACK(on_button_save_subject_clicked),
 						NULL);
+	g_signal_connect(G_OBJECT(tree_view_subject_list), "row-activated",
+						G_CALLBACK(on_tree_view_subject_list_row_activated),
+						(gpointer)entry_subject);
 
 	return hbox;
 }
@@ -119,13 +130,15 @@ static GtkWidget *create_text_view_subject_list(void)
 	store_subject_list = gtk_list_store_new(2, G_TYPE_STRING, G_TYPE_STRING);
 	load_subject_table(store_subject_list);
 	tree_view = gtk_tree_view_new();
+	gtk_tree_view_set_model(GTK_TREE_VIEW(tree_view),
+		GTK_TREE_MODEL(store_subject_list));
+	g_object_unref(store_subject_list);
 	gtk_tree_view_set_grid_lines(GTK_TREE_VIEW(tree_view),
 		GTK_TREE_VIEW_GRID_LINES_BOTH);
 	g_object_set(tree_view, "activate-on-single-click", TRUE, NULL);
 	selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(tree_view));
 	gtk_tree_selection_set_mode(selection, GTK_SELECTION_BROWSE);
-	gtk_tree_view_set_model(GTK_TREE_VIEW(tree_view),
-		GTK_TREE_MODEL(store_subject_list));
+
 	for (int i = 0; i < sizeof headers / sizeof headers[0]; i++)
 	{
 		render = gtk_cell_renderer_text_new();
@@ -241,6 +254,22 @@ static void show_message_box(const char *message)
 	gtk_window_set_title(GTK_WINDOW(dialog), MAIN_WINDOW_TITLE);
 	gtk_dialog_run(GTK_DIALOG(dialog));
 	gtk_widget_destroy(dialog);
+}
+
+static void on_tree_view_subject_list_row_activated(GtkTreeView *tree_view,
+	GtkTreePath *path, GtkTreeViewColumn *column, gpointer user_data)
+{
+	GtkTreeIter		iter;
+	GtkTreeModel	*model;
+	const gchar		*id;
+	const gchar		*subject;
+
+	model = gtk_tree_view_get_model(tree_view);
+	if (gtk_tree_model_get_iter(model, &iter, path))
+		gtk_tree_model_get(model, &iter, 0, &id, 1, &subject, -1);
+
+	selected_subject_id = (int) g_ascii_strtoll(id, NULL, 10);
+	gtk_entry_set_text(GTK_ENTRY(user_data), subject);
 }
 
 static int add_subject_to_store_cb(void *store, int col_count,
